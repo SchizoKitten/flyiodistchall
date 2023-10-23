1bb767b86c43125556a219286fa14c566ade6c81
SchizoKitten <rood19349@gmail.com>
2023-10-24 02:23:26 +0300

    Add some structs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+#![allow(dead_code)]
+struct Node{
+    id: usize,
+    
+}
+
+//#[derive(Deserialize, Serialize)]
+struct Message{
+    src: usize,
+    dest: usize,
+    body: Body,
+}
+
+//#[derive(Deserialize, Serialize)]
+//#[serde(tag = "type")]
+//serde flatten?
+enum Body{
+    Echo(Echo),
+    EchoOk(EchoOk)
+}
+
+struct Echo{
+//#[derive(Deserialize)]
+    r#type: String,
+    msg_id: usize,
+    echo: String,
+}
+
+struct EchoOk{
+//#[derive(Serialize)]
+    r#type: String,
+    msg_id: usize,
+    echo: String,
+}
+
 fn main() {
     println!("Hello, world!");
 }
