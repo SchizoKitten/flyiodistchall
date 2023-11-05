69a1c123de1f4a91a5f3e63ed5927c516295c0c5
SchizoKitten <rood19349@gmail.com>
2023-11-05 16:48:43 +0300

    Add in_reply_to to answer body

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,12 @@ fn main() {
     let mut n = Node::new(input, output);
     let key: &'static str = "\"echo\"";
     n.handler(key, Box::new(|message|{
-        let body = message.get_body_mut_ref();
-        *body.get_mut("type").unwrap() = "\"echo_ok\"".to_string();
-        body.insert(
+        let new_body = message.get_body_mut_ref();
+        *new_body.get_mut("type").unwrap() = "\"echo_ok\"".to_string();
+        new_body.insert(
             "in_reply_to".to_string(),
-            body.get("msg_id").unwrap().to_string());
-        body.remove("msg_id");
+            new_body.get("msg_id").unwrap().to_string());
+        new_body.remove("msg_id");
     }));
     let key: &'static str = "\"generate\"";
     let mut counter = 0;
@@ -20,6 +20,9 @@ fn main() {
         let mut new_body: HashMap<String, String> = HashMap::new();
         new_body.insert("type".to_string(), "\"generate_ok\"".to_string());
         new_body.insert("id".to_string(), counter.to_string());
+        new_body.insert(
+            "in_reply_to".to_string(),
+            message.get_body_ref().get("msg_id").unwrap().to_string());
         counter += 1;
         *message.get_body_mut_ref() = new_body;
     }));
